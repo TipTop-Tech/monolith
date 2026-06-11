cf00ace9c8f6f49a11e027ab8bd77d4304c3a592
Andy Sun <andymsun0@gmail.com>
2026-06-11 15:23:59 -0400

    fix(mobile): safe area clipping

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -14,7 +14,7 @@ export function Layout() {
 
   return (
     <WorkoutProvider>
-      <div className="h-full flex flex-col min-h-0">
+      <div className="h-full flex flex-col min-h-0 pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]"> 
         <main className="flex-1 min-h-0 overflow-auto">
           <Outlet />
         </main>
