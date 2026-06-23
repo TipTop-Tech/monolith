2810a1b4b75631dbcf18fdf22ad7541bc674caa9
NeonGems65 <ishaankumarb65@gmail.com>
2026-06-23 13:16:08 -0400

    Enable Web Analytics in Vercel

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,14 @@ import { router } from "./routes";
 import { AuthProvider } from "./context/AuthContext";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <AuthProvider>
+
       <RouterProvider router={router} />
+      <Analytics />
     </AuthProvider>
   );
 }
