786a0e5cef21d3368561e4fb1cb5df7838c2601c
lucaszli <lucaszli2025@gmail.com>
2026-07-29 17:56:37 -0400

    Rebuild app with agent selector

--- dist/assets/web-u-WJpQ9C.js
+++ dist/assets/web-u-WJpQ9C.js
@@ -1,4 +1,4 @@
-import{W as z}from"./index-C9KmK_HR.js";var _={exports:{}};/*!
+import{W as z}from"./index-DDC_A_tm.js";var _={exports:{}};/*!
   * $script.js JS loader & dependency manager
   * https://github.com/ded/script.js
   * (c) Dustin Diaz 2014 | License MIT
